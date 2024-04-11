5bac681b495ef87d7a2397fbe77b029f86494c81
koczankr <koczankr@gmail.com>
2024-04-11 16:38:50 +0200

    ingatlanok

---
+++ autokereskedes.js
@@ -0,0 +1,196 @@
+const autok = [
+    {
+      "gyartmany": "Chevrolet",
+      "tipus": "Malibu",
+      "uzemanyag": "Dizel",
+      "eletkor": 7
+    },
+    {
+      "gyartmany": "Toyota",
+      "tipus": "Corolla",
+      "uzemanyag": "Benzin",
+      "eletkor": 4
+    },
+    {
+      "gyartmany": "Ford",
+      "tipus": "Fusion",
+      "uzemanyag": "Elektromos",
+      "eletkor": 10
+    },
+    {
+      "gyartmany": "Honda",
+      "tipus": "Civic",
+      "uzemanyag": "Benzin",
+      "eletkor": 2
+    },
+    {
+      "gyartmany": "Volkswagen",
+      "tipus": "Passat",
+      "uzemanyag": "Dizel",
+      "eletkor": 8
+    },
+    {
+      "gyartmany": "Hyundai",
+      "tipus": "Elantra",
+      "uzemanyag": "Benzin",
+      "eletkor": 5
+    },
+    {
+      "gyartmany": "Nissan",
+      "tipus": "Altima",
+      "uzemanyag": "Elektromos",
+      "eletkor": 3
+    },
+    {
+      "gyartmany": "Mercedes-Benz",
+      "tipus": "C-Class",
+      "uzemanyag": "Dizel",
+      "eletkor": 9
+    },
+    {
+      "gyartmany": "BMW",
+      "tipus": "3 Series",
+      "uzemanyag": "Benzin",
+      "eletkor": 6
+    },
+    {
+      "gyartmany": "Audi",
+      "tipus": "A4",
+      "uzemanyag": "Elektromos",
+      "eletkor": 1
+    },
+    {
+      "gyartmany": "Lexus",
+      "tipus": "ES",
+      "uzemanyag": "Benzin",
+      "eletkor": 7
+    },
+    {
+      "gyartmany": "Kia",
+      "tipus": "Optima",
+      "uzemanyag": "Dizel",
+      "eletkor": 4
+    },
+    {
+      "gyartmany": "Subaru",
+      "tipus": "Legacy",
+      "uzemanyag": "Elektromos",
+      "eletkor": 10
+    },
+    {
+      "gyartmany": "Mazda",
+      "tipus": "Mazda6",
+      "uzemanyag": "Benzin",
+      "eletkor": 2
+    },
+    {
+      "gyartmany": "Buick",
+      "tipus": "Regal",
+      "uzemanyag": "Dizel",
+      "eletkor": 8
+    },
+    {
+      "gyartmany": "Chrysler",
+      "tipus": "300",
+      "uzemanyag": "Benzin",
+      "eletkor": 5
+    },
+    {
+      "gyartmany": "Jaguar",
+      "tipus": "XE",
+      "uzemanyag": "Elektromos",
+      "eletkor": 3
+    },
+    {
+      "gyartmany": "Volvo",
+      "tipus": "S60",
+      "uzemanyag": "Dizel",
+      "eletkor": 9
+    },
+    {
+      "gyartmany": "Infiniti",
+      "tipus": "Q50",
+      "uzemanyag": "Benzin",
+      "eletkor": 6
+    },
+    {
+      "gyartmany": "Tesla",
+      "tipus": "Model 3",
+      "uzemanyag": "Elektromos",
+      "eletkor": 1
+    },
+    {
+      "gyartmany": "Acura",
+      "tipus": "ILX",
+      "uzemanyag": "Benzin",
+      "eletkor": 7
+    },
+    {
+      "gyartmany": "Genesis",
+      "tipus": "G70",
+      "uzemanyag": "Dizel",
+      "eletkor": 4
+    },
+    {
+      "gyartmany": "Porsche",
+      "tipus": "Panamera",
+      "uzemanyag": "Elektromos",
+      "eletkor": 10
+    },
+    {
+      "gyartmany": "Mini",
+      "tipus": "Cooper",
+      "uzemanyag": "Benzin",
+      "eletkor": 2
+    },
+    {
+      "gyartmany": "Land Rover",
+      "tipus": "Range Rover",
+      "uzemanyag": "Dizel",
+      "eletkor": 8
+    },
+    {
+      "gyartmany": "Jeep",
+      "tipus": "Grand Cherokee",
+      "uzemanyag": "Benzin",
+      "eletkor": 5
+    },
+    {
+      "gyartmany": "Fiat",
+      "tipus": "500",
+      "uzemanyag": "Elektromos",
+      "eletkor": 3
+    },
+    {
+      "gyartmany": "Mitsubishi",
+      "tipus": "Lancer",
+      "uzemanyag": "Benzin",
+      "eletkor": 9
+    },
+    {
+      "gyartmany": "Ram",
+      "tipus": "1500",
+      "uzemanyag": "Dizel",
+      "eletkor": 6
+    }
+  ];
+/* 
+/api/autok
+összes autó összes adata
+
+/api/auto/:id
+id autó összes adata
+
+/api/markak
+sorolja fel az autómárkákat (midnegyiket csak egyszer)
+
+/api/marka/:marka
+szűrjön le az adott márkára
+
+/api/top3
+mutassa meg a 3 legfiatalabb autót
+
+*/
+
+
+  
